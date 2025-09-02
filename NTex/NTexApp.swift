0263991f5a94fa95a165ca6626c5d600bad93434
watarudishihara <wataru203@gmail.com>
2025-09-02 15:49:13 +0900

    Initial Commit

---
+++ NTex/NTexApp.swift
@@ -0,0 +1,17 @@
+//
+//  NTexApp.swift
+//  NTex
+//
+//  Created by Wataru Ishihara on 9/2/25.
+//
+
+import SwiftUI
+
+@main
+struct NTexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
